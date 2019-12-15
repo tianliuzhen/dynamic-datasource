fbd0b5cfed5f9a20b044a0c763ce7c7b9a7d900a
tianliuzhen <746033097@qq.com>
2019-12-15 23:02:42 +0800

    使用 gateway（不能使用最新的2.2.2版本会发生冲突）记录gateway采坑 管理 swagger2 ，并且美观 ui 访问美化url：http://localhost:8883/doc.html 访问原url：http://localhost:8883/swagger-ui.html

--- dynamic-data-mybatis/src/main/java/com/aaa/dynamic_data_mybatis/DynamicDataMybatisApplication.java
+++ dynamic-data-mybatis/src/main/java/com/aaa/dynamic_data_mybatis/DynamicDataMybatisApplication.java
@@ -2,8 +2,10 @@ package com.aaa.dynamic_data_mybatis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DynamicDataMybatisApplication {
 
     public static void main(String[] args) {
